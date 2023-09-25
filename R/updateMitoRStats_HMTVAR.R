e81e6b3bb4c0d06ad7b842af5379f79935274da9
DanielaOrschanski <100207075+DanielaOrschanski@users.noreply.github.com>
2023-09-25 12:31:11 -0300

    Update updateMitoRStats_HMTVAR.R
    cambiamos view_patient por update_freq

--- R/updateMitoRStats_HMTVAR.R
+++ R/updateMitoRStats_HMTVAR.R
@@ -65,61 +65,6 @@ updateMitoRStats_HMTVAR <- function(AddToPatients = FALSE){
   saveRDS(RDS_DB, sprintf("%s/RDS_DB.rds", mitor_db))
 }
 
-#' @title Adds or updates MitoR DB frequencies to the patient report
-#' @description Takes the MitoR DB frequencies from all the previous analysis and loads them into the patient report.
-#' It is important that you already have the analysis previously made. Otherwise, the function will analyze the patient from the VCF.
-#' If you did not keep the XLSX report nor the VCF file, you must do the analysis again with the patient's fasta/fastq files or its BAM file.
-#' @param numPaciente Patient ID.
-#' @return Updated report and a message telling the path it is saved at.
-#' @export
-#' @examples
-#' update_freq_MitoR("47286")
-
-update_freq_MitoR <- function(numPaciente) {
-  # Adds or updates the MitoR frequencies of the patient analysis
-  mitor_db <- sprintf("%s/mitorDB/DB", Sys.getenv('R_LIBS_USER'))
-  RDS_DB <- readRDS(sprintf("%s/RDS_DB.rds", mitor_db))
-
-  Freq_MitoR <- RDS_DB$Freq_MitoR[[1]]
-
-  patient_XLSX_file <- RDS_DB[[numPaciente]]["XLSX_Path"]
-  # Reads the xlsx patient analysis: 1 sheet: mutations - 2 sheet: softwares
-  # Add frequencies to 1. Add the update row to 2
-  paciente <- openxlsx::read.xlsx(patient_XLSX_file, sheet = 1)
-  softwares <- openxlsx::read.xlsx(patient_XLSX_file, sheet = 2)
-
-  # Takes the mutations of the patient
-  mutations <- RDS_DB[[numPaciente]]["Mutations"]
-  toAddFreq <- strsplit(unlist(mutations, use.names = FALSE), "/")
-
-  # Save in a new variable the MitoR DB frequencies of the mutations found in the patient
-  freqMut_MitoRStats <- c()
-  for (i in (1:length(toAddFreq))){
-    freqMut_MitoRStats <- c(freqMut_MitoRStats, Freq_MitoR[(Freq_MitoR$POS == as.integer(toAddFreq[[i]][2])) & (Freq_MitoR$ALT == toAddFreq[[i]][3]), ]$Freq_MitoR)
-  }
-
-  if ("Freq_MitoR" %in% colnames(paciente)){ # If its an update of the frequency
-    paciente$Freq_MitoR <- freqMut_MitoRStats
-    softwares$Last_Upd_FreqMitoR <- format(Sys.Date(), "%d/%m/%Y")
-  } else{ # If this is the first time it adds the frequency
-    paciente <- cbind(paciente, Freq_MitoR = freqMut_MitoRStats)
-    softwares <- cbind(softwares, Last_Upd_FreqMitoR = format(Sys.Date(), "%d/%m/%Y"))
-  }
-
-  SoftData[SoftData$Paciente == as.integer(numPaciente), ]$Last_Update <<- format(Sys.Date(), "%d/%m/%Y")
-
-  openxlsx::write.xlsx(paciente, patient_XLSX_file, sheetName =  sprintf("%s", numPaciente), rowNames = FALSE)
-  openxlsx::write.xlsx(softwares, patient_XLSX_file, sheetName =  "Software", rowNames = FALSE, overwrite = TRUE)
-
-  openxlsx::write.xlsx(mitoRStats, MitoRStats_path, sheetName = "MitoRStats", rowNames = FALSE)
-  openxlsx::write.xlsx(SoftData, MitoRStats_path, sheetName = "SoftData", rowNames = FALSE, overwrite = TRUE)
-
-  return(sprintf("MitoR frequencies updated. Find the report at: %s", MitoRStats_path))
-}
-
-
-
-# ----
 
 #' @title View Patient's Report
 #' @description Downloads the xlsx report of a specific patient with the data updated.
@@ -128,27 +73,18 @@ update_freq_MitoR <- function(numPaciente) {
 #' @examples
 #' view_patient()
 
-view_patient <- function() {
-  # Choose the directory where the patient's MitoR directory is found
-  select <- rstudioapi::selectDirectory(
-    caption = "Select Directory",
-    label = "Select",
-    path = "/home/biomolecular/DATA/NGS/MITOCONDRIAL/Pacientes/"
-  )
+update_freq_MitoR <- function(path_dir) {
 
-  if(length(select) == 0){ stop("Not selected") }
-
-  if (dir.exists(paste(select, "/MitoR", sep = ""))) {
-    file_list <- list.files(paste(select, "/MitoR", sep=""))
+  # Checks if the MitoR directory exists. If not the function is cancelled
+  if (dir.exists(paste(path_dir, "/MitoR", sep = ""))) {
+    file_list <- list.files(paste(path_dir, "/MitoR", sep=""))
     path_report <- file_list[endsWith(file_list, "MitoR_Report.xlsx")]
-    patient_XLSX_file <- sprintf("%s/MitoR/%s", select, path_report)
-
+    patient_XLSX_file <- sprintf("%s/MitoR/%s", path_dir, path_report)
   } else {
-    stop("This patient hasn't been analyzed. Please use Run_MitoR or select anaother patient.")
+    stop("This patient hasn't been analyzed. Please use Run_MitoR or select another patient.")
   }
 
-  numPaciente <- basename(select)
-  #patient_XLSX_file <- sprintf("%s/MitoR/%s_MitoR_Report.xlsx", select, numPaciente)
+  numPaciente <- basename(path_dir)
 
   # Adds or updates the MitoR frequencies of the patient analysis
   mitor_db <- sprintf("%s/mitorDB/DB", Sys.getenv('R_LIBS_USER'))
